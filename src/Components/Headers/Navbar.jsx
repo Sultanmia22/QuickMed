58de0ec727120e9bf631c1cb841e5793889a4e88
MD Sultan Mia <sultanmia5732@gmail.com>
2026-02-01 23:52:17 +0600

    create navbar structure

--- src/Components/Headers/Navbar.jsx
+++ src/Components/Headers/Navbar.jsx
@@ -1,51 +1,50 @@
-import React from 'react'
+import React, { useState } from 'react'
+import { FaClinicMedical } from 'react-icons/fa'
 import { Link, NavLink } from 'react-router'
-import logoImage from '../../assets/quickmedi-logo.png'
+
+
 const Navbar = () => {
 
-const navLinks = [
-  { name: 'Home', path: '/' },
-  { name: 'Find Doctors', path: '/find-doctors' },
-  { name: 'About Us', path: '/about' },
-  { name: 'Contact', path: '/contact' },
-  { name: 'How It Works', path: '/how-work' },
-]
+  const navLinks = [
+    { name: 'Home', path: '/' },
+    { name: 'Find Doctors', path: '/find-doctors' },
+    { name: 'About Us', path: '/about' },
+    { name: 'Contact', path: '/contact' },
+    { name: 'How It Works', path: '/how-work' },
+  ]
+
 
   return (
-    <div>
-      <div className="navbar bg-base-100 shadow-sm">
-  <div className="navbar-start">
-    <div className="dropdown">
-      <div tabIndex={0} role="button" className="btn btn-ghost lg:hidden">
-        <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"> <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h8m-8 6h16" /> </svg>
-      </div>
-      <ul
-        tabIndex="-1"
-        className="space-x-5 dropdown-content bg-base-100 rounded-box z-1 mt-3 w-52 p-2 shadow">
-        {
-          navLinks?.map( nav => 
-            <NavLink key={nav.path} to={nav.path}>{nav.name}</NavLink>
-           )
-        }
-      </ul>
-    </div>
-    <Link className='flex items-center gap-2'>
-    <h2 className='text-xl font-bold text-primary'>QuickMed</h2>
-    </Link>
-  </div>
-  <div className="navbar-center hidden lg:flex">
-    <ul className="space-x-5">
-      {
-          navLinks?.map( nav => 
-            <NavLink key={nav.path} to={nav.path}>{nav.name}</NavLink>
-           )
-        }
-    </ul>
-  </div>
-  <div className="navbar-end">
-    <a className="btn">Button</a>
-  </div>
-</div>
+    <div className='bg-base-100 py-5 border-b-2 border-primary w-full'>
+      <nav className=' w-11/12 md:w-10/12 mx-auto flex justify-between items-center'>
+
+        {/* Logo */}
+        <div className=''>
+          <div className=' w-full absolute top-21 left-0 md:hidden'>
+            <ul className='flex flex-col gap-5 bg-base-100  py-4 px-5  z-20 shadow-xl text-center'>
+              {
+                navLinks.map(link => <NavLink className={({ isActive }) => isActive ? ' border border-primary/25 py-2 text-primary rounded-md' : 'text-primary rounded-md'} key={link.path} to={link.path}> {link.name} </NavLink>)
+              }
+            </ul>
+          </div>
+          <Link className='flex items-center gap-1'>
+            <span><FaClinicMedical className='text-primary' size={26} /></span>
+            <h2 className='text-xl text-primary font-bold'> Quick<span className='text-secondary'>M</span>ed </h2>
+          </Link>
+        </div>
+
+        {/* Nav Links Desktop */}
+        <ul className='space-x-5 text-primary hidden md:flex'>
+          {
+            navLinks.map(link => <NavLink className={({ isActive }) => isActive ? 'border-b-2 border-secondary' : ''} key={link.path} to={link.path}> {link.name} </NavLink>)
+          }
+        </ul>
+
+        {/* PROFILE AND AUTH BUTTONS */}
+        <div>
+          <button className='btn btn-outline btn-primary mr-2'> Login </button>
+        </div>
+      </nav>
     </div>
   )
 }
